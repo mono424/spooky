e73612f329cf31325bc8d264c00e19eb6b6a71ab
Khadim Fall <khadimfall0602@gmail.com>
2026-01-05 22:24:34 +0100

    update to pino

--- packages/core/src/services/database/local.ts
+++ packages/core/src/services/database/local.ts
@@ -4,19 +4,23 @@ import { SpookyConfig } from '../../types.js';
 import { Logger } from '../logger.js';
 import { AbstractDatabaseService } from './database.js';
 
-const printDiagnostic = ({ key, type, phase, ...other }: Diagnostic) => {
-  if (phase === 'progress' || phase === 'after') {
-    console.log(`[SurrealDB:LOCAL] [${key}] ${type}:${phase}\n${JSON.stringify(other, null, 2)}`);
-  }
-};
-
 export class LocalDatabaseService extends AbstractDatabaseService {
   private config: SpookyConfig<any>['database'];
 
   constructor(config: SpookyConfig<any>['database'], logger: Logger) {
     super(
       new Surreal({
-        engines: applyDiagnostics(createWasmEngines(), printDiagnostic),
+        engines: applyDiagnostics(
+          createWasmEngines(),
+          ({ key, type, phase, ...other }: Diagnostic) => {
+            if (phase === 'progress' || phase === 'after') {
+              logger.info(
+                { ...other, key, type, phase, service: 'surrealdb:local' },
+                `[SurrealDB:LOCAL] [${key}] ${type}:${phase}`
+              );
+            }
+          }
+        ),
       }),
       logger
     );
--- packages/core/src/services/database/remote.ts
+++ packages/core/src/services/database/remote.ts
@@ -9,18 +9,24 @@ import { SpookyConfig } from '../../types.js';
 import { Logger } from '../logger.js';
 import { AbstractDatabaseService } from './database.js';
 
-const printDiagnostic = ({ key, type, phase, ...other }: Diagnostic) => {
-  if (phase === 'progress' || phase === 'after') {
-    console.log(`[SurrealDB:REMOTE] [${key}] ${type}:${phase}\n${JSON.stringify(other, null, 2)}`);
-  }
-};
-
 export class RemoteDatabaseService extends AbstractDatabaseService {
   private config: SpookyConfig<any>['database'];
 
   constructor(config: SpookyConfig<any>['database'], logger: Logger) {
     super(
-      new Surreal({ engines: applyDiagnostics(createRemoteEngines(), printDiagnostic) }),
+      new Surreal({
+        engines: applyDiagnostics(
+          createRemoteEngines(),
+          ({ key, type, phase, ...other }: Diagnostic) => {
+            if (phase === 'progress' || phase === 'after') {
+              logger.info(
+                { ...other, key, type, phase, service: 'surrealdb:remote' },
+                `[SurrealDB:REMOTE] [${key}] ${type}:${phase}`
+              );
+            }
+          }
+        ),
+      }),
       logger
     );
     this.config = config;
