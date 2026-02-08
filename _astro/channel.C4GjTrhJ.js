6ed2002e29fbd27c96ae5fa0f2a8d9c575e11dfa
mono424 <khadimfall0602@gmail.com>
2026-02-08 15:07:43 +0000

    Deploying to gh-pages from @ mono424/spooky@6720a04e3a870ff4f643797d4f461e9005aa4249 🚀

---
+++ _astro/channel.C4GjTrhJ.js
@@ -0,0 +1,1 @@
+import{af as o,ag as n}from"./DocsLayout.astro_astro_type_script_index_1_lang.CWlSDCTd.js";const t=(a,r)=>o.lang.round(n.parse(a)[r]);export{t as c};
--- _astro/channel.CLxlaTxX.js
+++
@@ -1,1 +0,0 @@
-import{ai as o,aj as n}from"./DocsLayout.astro_astro_type_script_index_1_lang.CSc0ynAD.js";const t=(a,r)=>o.lang.round(n.parse(a)[r]);export{t as c};
